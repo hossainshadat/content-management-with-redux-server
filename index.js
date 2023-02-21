d94e4e3ef4f9de4c2f9b83d965929d9d130bcacd
hossainshadat <hossainshadat501@gmail.com>
2023-02-22 00:35:10 +0600

    mongodb connected successfully

--- index.js
+++ index.js
@@ -1,12 +1,57 @@
 const express = require("express");
 require("dotenv").config();
-const cors = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7lbeksz.mongodb.net/test`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function dbConnect() {
+  try {
+    await client.connect();
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log(error.name, error.message);
+  }
+}
+
+dbConnect();
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+const productCollection = client.db("moontech").collection("product");
+
+app.get("/products", async (req, res) => {
+  try {
+    let cursor = productCollection.find({});
+    const products = await cursor.toArray();
+
+    res.send({
+      success: true,
+      message: "Successfully get the Data",
+      data: products,
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: error.message,
+    });
+  }
 });
 
 app.listen(process.env.PORT || 5000, () => {
